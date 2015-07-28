fa4ae545cd23a3e1fdf9eea8ac360cca4d929f28
shunkin <shunkin@sfc.wide.ad.jp>
2015-07-28 23:52:49 +0900

    hoge

--- TexasHoldem/app.js
+++ TexasHoldem/app.js
@@ -26,8 +26,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 
-app.get('/card', function(req, res, next){
+app.get('/API/v1.0/Tray/P1/1', function(req, res, next){
 	res.render('card', { Suits:'1', Num:'4' });
+});
+app.get('/API/v1.0/Tray/P1/2', function(req, res, next){
+	res.render('card', { Suits:'2', Num:'3' });
+});
+app.get('/API/v1.0/Tray/P2/1', function(req, res, next){
+	res.render('card', { Suits:'3', Num:'6' });
+});
+app.get('/API/v1.0/Tray/P2/2', function(req, res, next){
+	res.render('card', { Suits:'1', Num:'19' });
 });
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
